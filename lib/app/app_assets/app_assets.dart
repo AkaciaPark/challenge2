6efe65c9c8a6b3e8b04eec833965a8aabd970c9c
Akacia <akaciapark@gmail.com>
2023-03-06 17:46:31 -0300

    did the static class for the images

---
+++ lib/app/app_assets/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  static const avataraAppbarImage =
+      'assets/images/first_screen_images/woman.jpg';
+  static const menuIconAppbarImage = 'assets/images/icons/menu_icon.png';
+  static const filterIconTabbarImage = 'assets/images/icons/filter_icon.png';
+  static const filterDogsTabbarImage = 'assets/images/icons/dog_icon.png';
+  static const filterCatsTabbarImage = 'assets/images/icons/cat_icon.png';
+  static const filterBirdsTabbarImage = 'assets/images/icons/bird_icon.png';
+  static const backIconAppbarImage = 'assets/images/icons/goback_icon.png';
+  static const genderIconImage = 'assets/images/icons/male_gender_icon.png';
+}
